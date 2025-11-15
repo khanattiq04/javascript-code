9dd8eee9f03b806e58762519e9c5557ce3dffa74
khanattiq04 <khanattiq04@gmail.com>
2025-11-15 09:37:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=123456
+let accountEmail="attiqkhan006@gmail.com"
+var accountPassword="112233"
+accountCity="attock"
+let accountState
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using node.js, not browser
+
+let name="Attiq"
+let age=33
+let isLoggedIn=false
+
+
+//number = 2 to power 53
+//bigint
+//string=> ""
+//boolean = true/fasle
+//nul => standalone value
+//undefined =>
+//symbol=>unique
+
+console.log(typeof undefined)
+console.log(typeof null);
